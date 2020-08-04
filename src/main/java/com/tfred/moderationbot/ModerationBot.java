a738939d19e6633342dc6274cfab7dcc6ba3579e
Frederik <47461904+freeder1k@users.noreply.github.com>
2020-08-04 11:37:28 +0200

    commands moved to new class

--- src/main/java/com/tfred/moderationbot/ModerationBot.java
+++ src/main/java/com/tfred/moderationbot/ModerationBot.java
@@ -111,131 +111,8 @@ public class ModerationBot extends ListenerAdapter
 
             System.out.printf("[PRIV]<%s>: %s\n", author.getName(), msg);
         }
-
-        //Now that you have a grasp on the things that you might see in an event, specifically MessageReceivedEvent,
-        // we will look at sending / responding to messages!
-        //This will be an extremely simplified example of command processing.
-
-        //Remember, in all of these .equals checks it is actually comparing
-        // message.getContentDisplay().equals, which is comparing a string to a string.
-        // If you did message.equals() it will fail because you would be comparing a Message to a String!
-        if (msg.equals("!help")) {
-            //This will send a message, "pong!", by constructing a RestAction and "queueing" the action with the Requester.
-            // By calling queue(), we send the Request to the Requester which will send it to discord. Using queue() or any
-            // of its different forms will handle ratelimiting for you automatically!
-
-            channel.sendMessage("Help:\n-``!delreaction <emoji> <amount>``: delete all reactions with a specified emoji <amount> messages back (max 100).\n-``!modrole <add|remove|list> [role]``: add/remove a modrole or list the mod roles for this server.\n-``!nosalt``: toggle no salt mode.").queue();
-        }
-
-        else if (msg.startsWith("!delreaction ")) {
-            Guild guild = event.getGuild();
-            Member member = message.getMember();
-            if((member.hasPermission(Permission.ADMINISTRATOR) || (isModerator(guild.getId(), member)))) {
-                Member selfMember = guild.getSelfMember();
-
-                if (!selfMember.hasPermission(Permission.MESSAGE_MANAGE)) {
-                    channel.sendMessage("Please make sure to give me the manage messages permission!").queue();
-                    return; //We jump out of the method instead of using cascading if/else
-                }
-
-                String[] args = msg.split(" ");
-
-                if(args.length != 3) {
-                    channel.sendMessage("Invalid amount of arguments!").complete();
-                    return;
-                }
-
-                String emoji = args[1];
-
-                int amount ;
-                try {
-                    amount = Integer.parseInt(args[2]);
-                } catch (NumberFormatException e) {
-                    channel.sendMessage("Error parsing amount!").complete();
-                    return;
-                }
-                if(amount > 100 || amount < 1) {
-                    channel.sendMessage("Amount must be in range 1-100").complete();
-                    return;
-                }
-
-                channel.sendMessage("Removing reactions...").complete();
-
-                try {
-                    for (Message m : message.getChannel().getHistory().retrievePast(amount).complete()) {
-                        m.clearReactions(emoji).complete();
-                    }
-                } catch (ErrorResponseException e) {
-                    channel.sendMessage("Unknown emoji: " + emoji + ".\nFor custom emojis please add the ID after the emoji. Example: ``:emoji:123456789``.").queue();
-                    return;
-                }
-
-                channel.sendMessage("Finished removing reactions with " + emoji + ".").queue();
-            }
-        }
-
-        else if (msg.startsWith("!modrole ")) {
-            String guildID = event.getGuild().getId();
-            if((message.getMember().hasPermission(Permission.ADMINISTRATOR))) {
-                String[] args = msg.split(" ");
-
-                if(args.length < 2) {
-                    channel.sendMessage("Invalid amount of arguments!").complete();
-                    return;
-                }
-
-                if(args[1].equals("list")) {
-                    List<String> roles = serverdata.getModRoles(guildID);
-
-                    if(roles.isEmpty()) {
-                        channel.sendMessage("There are no moderator roles.").queue();
-                        return;
-                    }
-
-                    String res = "";
-                    for(String s: roles) {
-                        Role r = event.getGuild().getRoleById(s);
-                        if(r != null)
-                            res = res.concat(" ``" + r.getName() + "``,");
-                    }
-                    res = res.substring(0, res.length() - 1);
-
-                    channel.sendMessage("Moderator roles:" + res + ".").queue();
-                    return;
-                }
-
-                Role role;
-                try {
-                    role = message.getMentionedRoles().get(0);
-                } catch (IndexOutOfBoundsException e) {
-                    channel.sendMessage("Please mention a role!").complete();
-                    return;
-                }
-
-                if (args[1].equals("add")) {
-                    serverdata.addModRole(guildID, role.getId());
-                    channel.sendMessage("Added " + role.getName() + " to moderator roles.").queue();
-                } else if(args[1].equals("remove")) {
-                    serverdata.removeModRole(guildID, role.getId());
-                    channel.sendMessage("Removed " + role.getName() + " from moderator roles.").queue();
-                } else
-                    channel.sendMessage("Unknown action. Allowed actions: ``add, remove, list``.").queue();
-            }
-        }
-
-        else if (msg.equals("!nosalt")) {
-            String guildID = event.getGuild().getId();
-            Member member = message.getMember();
-            if((member.hasPermission(Permission.ADMINISTRATOR) || (isModerator(guildID, member)))) {
-                if (serverdata.isNoSalt(guildID)) {
-                    serverdata.setNoSalt(guildID, false);
-                    channel.sendMessage("No salt mode disabled.").queue();
-                } else {
-                    serverdata.setNoSalt(guildID, true);
-                    channel.sendMessage("No salt mode enabled!").queue();
-                }
-            }
-        }
+        if (msg.startsWith("!"))
+            Commands.process(event, serverdata);
 
         else if (msg.contains("\uD83E\uDDC2") || msg.contains("⏰")) {
             //Deletes messages with salt emoji
@@ -247,13 +124,5 @@ public class ModerationBot extends ListenerAdapter
                     message.delete().queue();
             }
         }
-    }
-
-    private boolean isModerator(String serverID, Member member) {
-        for(Role r: member.getRoles()) {
-            if(serverdata.isModRole(serverID, r.getId()))
-                return true;
-        }
-        return false;
     }
 }
